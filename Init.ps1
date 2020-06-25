15407586ede848b9ed3eb764e0a58384ffbd18e7
Vladimir Putin <vputin@krem.lin>
2020-06-25 16:19:53 -0500

    init

---
+++ Init.ps1
@@ -0,0 +1,2 @@
+Import-Module -Force ".\execution-frameworks\invoke-atomicredteam\Invoke-AtomicRedTeam.psd1"
+Import-Module -Force ".\execution-frameworks\invoke-atomicredteam\Invoke-AtomicRedTeam.psm1"
