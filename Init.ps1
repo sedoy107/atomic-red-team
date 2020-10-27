1d612b47da615f51698698cff7f1069ddf75abc9
Fire Power <firepower@threat-dev.sg>
2020-10-27 15:09:50 -0500

    Init script

--- Init.ps1
+++ Init.ps1
@@ -1,2 +1,5 @@
-Import-Module -Force ".\execution-frameworks\invoke-atomicredteam\Invoke-AtomicRedTeam.psd1"
-Import-Module -Force ".\execution-frameworks\invoke-atomicredteam\Invoke-AtomicRedTeam.psm1"
+Import-Module -Force ".\execution-frameworks\invoke-atomicredteam\Invoke-AtomicRedTeam.psd1" -ErrorAction Stop -Verbose
+Import-Module -Force ".\execution-frameworks\invoke-atomicredteam\Invoke-AtomicRedTeam.psm1" -ErrorAction Stop -Verbose
+
+Write-Host -ForegroundColor Green "Atomics Execution Framework is ready for use..."
+Write-Host -ForegroundColor Green "Use `"Invoke-AtomicTest`" to execute atomics."
